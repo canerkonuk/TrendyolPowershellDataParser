a21e0652ccf0b059658fde58de5009e8e7ca0b44
Caner Konuk <42453830+canerkonuk@users.noreply.github.com>
2023-11-11 23:55:01 +0300

    Added PS script for getting products data from public API.

---
+++ TrendyolProductDataParser.ps1
@@ -0,0 +1,21 @@
+﻿# Create an empty array to store products data from responses:
+$products = @()
+
+# Initialize pageIndex for query:
+$pi = 1
+
+# Add product category from url:
+$pathModel="tablet-x-c103665"
+
+do {
+    # Parse json content
+    $url = "https://public.trendyol.com/discovery-mweb-searchgw-service/api/search-v2/products/?storeId=&pathModel=%2F$($pathModel)&pi=$($pi)&pageSize=100&sst=BEST_SELLER&language=tr"
+    $response = (Invoke-WebRequest -Uri $url -Method Get).Content | ConvertFrom-Json
+    $products+=$response.products
+
+    # Increment pi
+    $pi++
+    Write-Host($products.Count)
+
+    } while ($response.appliedSearchStrategy -ne "NO_RESULT" -and $products.Count -ne 5000)
+
